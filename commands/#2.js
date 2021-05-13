0a5dc1e5e3766fd16eb1ca842c73420b8136e886
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2021-05-14 00:59:46 +0300

    Create #2.js

---
+++ commands/#2.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'lock',
+	description: 'Locks the channel',
+	execute(client, message, args) {    // The return function will stop the code from working and returns the continuing line.
+if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('No perms!');
+const role = guild.roles.find("name", '@everyone');
+message.channel.overwritePermissions(role,{ 'SEND_MESSAGES': false });
+  }
+}
+
+// Unlock command in #3!
