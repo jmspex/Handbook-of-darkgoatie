7aeebbcd357879f4eb7398d8d56095f659b9061c
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2021-05-14 21:44:06 +0300

    Create lock.js

---
+++ guide/fully-made-example-bot/commands/lock.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'lock',
+	aliases: ['l'],
+	description: 'Locks the channel',
+	execute(client, message, args) {    // The return function will stop the code from working and returns the continuing line.
+if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('No perms!');
+const role = guild.roles.find("name", '@everyone');
+    try { // The try will prevent the bot from crashing if doesn't have perms, is quarantined etc.
+message.channel.overwritePermissions(role,{ 'SEND_MESSAGES': true });
+message.channel.send('The channel has been locked!') // Here we notify that the channel perms were successfully modified.
+    } catch(e) { // This part will report the error if an error occurs.
+      message.channel.send('Error!'/*This is the part which will send us the error -> */+ e)
+    }
+  }
+}
