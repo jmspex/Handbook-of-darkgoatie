02f32c0c782742389cc96a1275572628ed918b01
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2021-05-17 22:23:57 +0300

    Create gend.js

---
+++ bots/Giveaway Bot/commands/gend.js
@@ -0,0 +1,41 @@
+const ms = require('ms');
+
+module.exports = {
+	name: 'end',
+    aliases: ['end-giveaway','gend'],
+	description: 'Ends a giveaway',
+	execute(client, message, args) {
+    if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name.toLowerCase() === "Giveaway Manager")){
+        return message.channel.send(':boom: You need to have the \`MANAGE_MESSAGES\` permissions or a role named Giveaway Manager to end giveaways.');
+    }
+
+    if(!args[0]){
+        return message.channel.send(':boom: Uh oh, I couldn\'t find that message! Try again!');
+    }
+
+    let giveaway = 
+    client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) ||
+    client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
+
+    if(!giveaway){
+        return message.channel.send(':boom: Hm. I can\'t seem to find a giveaway for `'+ args.join(' ') + '`.');
+    }
+    client.giveawaysManager.edit(giveaway.messageID, {
+        setEndTimestamp: Date.now()
+    })
+    .then(() => {
+        message.channel.send('Giveaway will end in less than '+(client.giveawaysManager.options.updateCountdownEvery/1000)+' seconds...');
+    })
+    .catch((e) => {
+        if(e.startsWith(`Giveaway with message ID ${giveaway.messageID} has already ended.`)){
+          
+            message.channel.send('This giveaway has already ended!');
+
+        } else {
+            console.error(e);
+            message.channel.send('An error occurred...');
+        }
+    });
+
+}
+}
